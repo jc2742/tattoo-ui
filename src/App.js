b52c8a8be7b407858a481df3c1661358ad8e2727
Jack Chen <jc2742@cornell.edu>
2023-12-22 22:35:23 -0500

    add analyis

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Layout from './components/Layout';
-import HomeContainer from './containers/HomeContainer';
+import logo from "./logo.svg";
+import "./App.css";
+import Layout from "./components/Layout";
+import HomeContainer from "./containers/HomeContainer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Layout>
       <HomeContainer />
+      <Analytics />
     </Layout>
   );
 }
