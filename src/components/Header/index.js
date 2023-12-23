be4592b551151cd07f42a6230f17222f39aead4e
Jack Chen <jc2742@cornell.edu>
2023-12-22 20:09:31 -0500

    add tiktok link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,18 +7,21 @@ import { Icons } from "../atomic/atoms/Icons";
 export default function Header() {
   return (
     <nav className="sticky top-0 bg-black w-full py-[32px] px-[48px] justify-between flex z-10">
-
       <a href="/" className="flex gap-4">
-        <h2 className="text-white text-2xl font-pacifico">
-          Chinatown Tattoo
-        </h2>
+        <h2 className="text-white text-2xl font-pacifico">Chinatown Tattoo</h2>
       </a>
       <Flex align="center" gap={10}>
-        {
-          NavIcons.map(element => {
-            return <a href={element.link}>{element.name.length > 0 ? <Icons name={element.name} /> : element.icon}</a>
-          })
-        }
+        {NavIcons.map((element) => {
+          return (
+            <a href={element.link}>
+              {element.name.length > 0 ? (
+                <Icons name={element.name} />
+              ) : (
+                element.icon
+              )}
+            </a>
+          );
+        })}
       </Flex>
     </nav>
   );
@@ -26,17 +29,18 @@ export default function Header() {
 
 const NavIcons = [
   {
-    name: '',
+    name: "",
     icon: <FiInstagram size={20} color="#FFF" />,
-    link: 'https://www.instagram.com/chinatowntattooles/?hl=en'
+    link: "https://www.instagram.com/chinatowntattooles/?hl=en",
   },
   {
-    name: '',
+    name: "",
     icon: <FaFacebook size={20} color="#FFF" />,
-    link: 'https://www.facebook.com/chinatowntattooles/'
+    link: "https://www.facebook.com/chinatowntattooles/",
   },
   {
-    name: '',
-    icon: <FaTiktok size={20} color="#FFF" />
-  }
-]
+    name: "",
+    icon: <FaTiktok size={20} color="#FFF" />,
+    link: "https://www.tiktok.com/@chinatowntattoo",
+  },
+];
